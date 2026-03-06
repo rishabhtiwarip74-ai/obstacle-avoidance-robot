962ea651262a733a4d8d741ae14c89e963e23f26
Rishabh Tiwari <rishabh.tiwarip74@gmail.com>
2026-03-06 16:19:46 +0530

    Add files via upload

---
+++ obstacle-avoidance-bot.ino
@@ -0,0 +1,126 @@
+#include <Servo.h>
+
+#define trigPin 9
+#define echoPin 10
+
+#define IN1 4
+#define IN2 5
+#define IN3 6
+#define IN4 7
+
+Servo myservo;
+
+long duration;
+int distance;
+
+void setup() {
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  myservo.attach(3);
+  myservo.write(90);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  distance = getDistance();
+
+  if(distance > 20)
+  {
+    moveForward();
+  }
+  else
+  {
+    stopRobot();
+    delay(300);
+
+    int leftDistance;
+    int rightDistance;
+
+    myservo.write(160);
+    delay(500);
+    leftDistance = getDistance();
+
+    myservo.write(20);
+    delay(500);
+    rightDistance = getDistance();
+
+    myservo.write(90);
+
+    if(leftDistance > rightDistance)
+    {
+      turnLeft();
+      delay(500);
+    }
+    else
+    {
+      turnRight();
+      delay(500);
+    }
+  }
+}
+
+int getDistance()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = duration * 0.034 / 2;
+
+  return distance;
+}
+
+void moveForward()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void moveBackward()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void turnLeft()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void turnRight()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void stopRobot()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
